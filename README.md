f5c8c206ac86e4bef2861103abe450b9b71386c3
justooney <web@justooney.com>
2022-10-11 12:50:29 -0500

    parked

---
+++ README.md
@@ -0,0 +1,3 @@
+React code for www.justooney.net
+
+All code copyright Justooney, 2020-22
---
+++ src/components/ParkingNotice.js
@@ -0,0 +1,7 @@
+import './ParkingNotice.css';
+
+const ParkingNotice = () => {
+  return <h1 className='parking_notice'>This domain is parked.</h1>;
+};
+
+export default ParkingNotice;
